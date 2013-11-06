bbf76c7f83c6a6164b52e1302f0a3a5edeb38dab
Sebastian Holzapfel <schnommus@gmail.com>
2013-11-06 15:40:05 +1100

    Added SFML dependencies; simple SFML test up

--- src/SFML_SignedDepthDemo.cpp
+++ src/SFML_SignedDepthDemo.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() {
-	std::cout << "Hey!" << std::endl;
-	return 0;
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
