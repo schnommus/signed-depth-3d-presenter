0c32623ff71272e031be4f11e485d74de934fb8f
Sebastian Holzapfel <schnommus@gmail.com>
2013-11-07 18:51:08 +1100

    Font details from textfile into memory

--- src/SFML_SignedDepthDemo.cpp
+++ src/SFML_SignedDepthDemo.cpp
@@ -1,7 +1,63 @@
 #include <SFML/Graphics.hpp>
 
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+std::vector<std::string> &split(std::string &s, char delim, std::vector<std::string> &elems) {
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+		if(!item.empty())
+			elems.push_back(item);
+    }
+    return elems;
+}
+
+
+std::vector<std::string> split(std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, elems);
+    return elems;
+}
+
+struct glyph {
+	int id;
+	int x, y, w, h;
+	float off;
+};
+
 int main()
 {
+	std::ifstream file;
+	file.open("../media/sdf1.txt", std::ios::in );
+	std::string line;
+
+	std::vector< glyph > glyphs;
+
+	while( getline (file, line) ) {
+      std::vector< std::string > elements = split(line, ' ');
+	  if( elements[0] == "char" ) {
+		  glyph g;
+		  std::istringstream idstr(split( elements[1], '=' )[1]); idstr >> g.id;
+		  std::cout << "id: " << g.id  << " \'" << (char)g.id << "\', ";
+		  std::istringstream xstr(split( elements[2], '=' )[1]); xstr >> g.x;
+		  std::cout << "x: " << g.x << ", ";
+		  std::istringstream ystr(split( elements[3], '=' )[1]); ystr >> g.y;
+		  std::cout << "y: " << g.y << ", ";
+		  std::istringstream wstr(split( elements[4], '=' )[1]); wstr >> g.w;
+		  std::cout << "w: " << g.w << ", ";
+		  std::istringstream hstr(split( elements[5], '=' )[1]); hstr >> g.h;
+		  std::cout << "h: " << g.h  << ", ";
+		  std::istringstream offstr(split( elements[8], '=' )[1]); offstr >> g.off;
+		  std::cout << "off: " << g.off << std::endl;
+		  glyphs.push_back(g);
+	  }
+    }
+	file.close();
+
+	
 	sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML_SignedDistance", sf::Style::Fullscreen);
     
 
@@ -28,7 +84,7 @@ int main()
                 window.close();
         }
 
-		sdfSprite.setScale( 1.5+sin(delta.getElapsedTime().asSeconds()), 1.5+sin(delta.getElapsedTime().asSeconds()) );
+		sdfSprite.setScale( 6+3*sin(delta.getElapsedTime().asSeconds()), 6+3*sin(delta.getElapsedTime().asSeconds()) );
 		sdfSprite.setRotation( delta.getElapsedTime().asSeconds()*45.0 );
 
 
